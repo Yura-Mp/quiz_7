f6e44676c29dcf1eb3ec2180d2dae01cdf98d558
Yura-Mp <e1b22079@oit.ac.jp>
2024-10-29 16:00:47 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/team7/quiz_7/Quiz7Application.java
@@ -0,0 +1,13 @@
+package oit.is.team7.quiz_7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Quiz7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Quiz7Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/team7/quiz_7/Quiz7ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.team7.quiz_7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Quiz7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
