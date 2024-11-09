767b6411bef537526249883d7e35257ab65da4c1
Yura-Mp <e1b22079@oit.ac.jp>
2024-11-09 17:52:10 +0900

    ユーザアカウントの枠組み・DBテーブル定義 の実装

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,12 @@
+/* schema.sql: 主にCREATE(テーブル作成&初期化)系のコマンドを記述．
+ * ※このsqlコマンドファイルは，サービス起動時に毎回適用されることを想定する．
+ * ・CREATE TABLE文の場合は，すでにあるテーブルに重ねて作成されることによるエラーを防ぐため，必ず存在判定をすることとする．
+*/
+
+-- ユーザアカウントデータ格納用テーブル (Author: Yura-Mp)
+CREATE TABLE IF NOT EXISTS userAccount (
+  userId VARCHAR PRIMARY KEY, -- ユーザID(主キー)．
+  userName VARCHAR NOT NULL, -- ユーザの名前．サービス上で表示される名前として用いられることを想定．
+  pass VARCHAR, -- アカウントのパスワード．データとしてはbcryptでハッシュ化されたデータを保持することを想定．NULL(パスワード未設定)である場合，パスワードなしでログインできるものとする．
+  available BOOLEAN DEFAULT TRUE -- アカウントが利用可能(有効)であるかを示す0/1フラグ．TRUEならば利用可能，FALSEならば利用不可能(アカウント停止)を示す．
+);
