0be7f8f9526d28e3fea312d6e80401f671a80d5c
e1b22061 <e1b22061@oit.ac.jp>
2024-11-14 17:43:22 +0900

    ユーザアカウントのスキーマ変更

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,8 +5,9 @@
 
 -- ユーザアカウントデータ格納用テーブル (Author: Yura-Mp)
 CREATE TABLE IF NOT EXISTS userAccount (
-  userId VARCHAR PRIMARY KEY, -- ユーザID(主キー)．
-  userName VARCHAR NOT NULL, -- ユーザの名前．サービス上で表示される名前として用いられることを想定．
-  pass VARCHAR, -- アカウントのパスワード．データとしてはbcryptでハッシュ化されたデータを保持することを想定．NULL(パスワード未設定)である場合，パスワードなしでログインできるものとする．
-  available BOOLEAN DEFAULT TRUE -- アカウントが利用可能(有効)であるかを示す0/1フラグ．TRUEならば利用可能，FALSEならば利用不可能(アカウント停止)を示す．
+  id IDENTITY, -- ユーザID(主キー)．
+  userName VARCHAR(20) UNIQUE NOT NULL, -- ユーザの名前．サービス上で表示される名前として用いられることを想定．
+  pass VARCHAR NOT NULL, -- アカウントのパスワード．データとしてはbcryptでハッシュ化されたデータを保持することを想定．NULL(パスワード未設定)である場合，パスワードなしでログインできるものとする．
+  available BOOLEAN DEFAULT TRUE, -- アカウントが利用可能(有効)であるかを示す0/1フラグ．TRUEならば利用可能，FALSEならば利用不可能(アカウント停止)を示す．
+  roles VARCHAR NOT NULL
 );
