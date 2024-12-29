6cd590ed3585d7b11abe79f743fd71cefd69288c
e1b22103 <e1b22103@oit.ac.jp>
2024-12-29 15:41:19 +0900

    ゲームルーム主催者開始前(待機)ページ(/gameroom/standby)でのゲーム中止処理の実装

--- src/main/java/oit/is/team7/quiz_7/model/PGameRoomManager.java
+++ src/main/java/oit/is/team7/quiz_7/model/PGameRoomManager.java
@@ -38,6 +38,10 @@ public class PGameRoomManager {
     this.belonging = belonging;
   }
 
+  public void removeGameRoom(long roomID) {
+    publicGameRooms.remove(roomID);
+  }
+
   public void removeParticipantFromGameRoom(long userID, long roomID) {
     belonging.remove(userID);
     PublicGameRoom gameRoom = publicGameRooms.get(roomID);
