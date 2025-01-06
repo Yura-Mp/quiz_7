29d172bbcde981b10d7d702ce2846243b7edc96c
Yura-Mp <e1b22079@oit.ac.jp>
2025-01-06 15:21:08 +0900

    バグ修正

--- src/main/resources/static/js/SseAutoRedirectToAnsResultPage.js
+++ src/main/resources/static/js/SseAutoRedirectToAnsResultPage.js
@@ -15,11 +15,9 @@ function SseAutoRedirectToAnsResultPage() {
     console.log("SseAutoRedirectToAnsResultPage: sse.onmessage Event received: " + event.data);
 
     try {
-      if(!isNaN(event.data)) {
-        const loc = "/playing/ans_result?" + event.data;
-        console.log("Redirect to " + loc);
-        window.location.href = loc;
-      }
+      const loc = "/playing/ans_result?" + event.data;
+      console.log("Redirect to " + loc);
+      window.location.href = loc;
 
     } catch(e) {
       console.error("Error by " + e);
