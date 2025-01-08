44ede40e7ca9b952674fc88f7f2a1a0e0c614315
Yura-Mp <e1b22079@oit.ac.jp>
2025-01-09 02:25:46 +0900

    close処理の追加-1

--- src/main/resources/static/js/SseAutoRedirectToAnswerPage.js
+++ src/main/resources/static/js/SseAutoRedirectToAnswerPage.js
@@ -1,28 +1,37 @@
-function SseAutoRedirectToAnswerPage() {
-  console.log("SseAutoRedirectToAnswerPage function is called. ")
+class SseAutoRedirectToAnswerPage {
+  sse = undefined;
 
-  let sse = new EventSource("/sse/transitToAnswer");
-  sse.onopen = function() {
-    console.info("SseAutoRedirectToAnserPage: SSE connection opened. ");
-  };
-  sse.onerror = function (error) {
-    console.error("SseAutoRedirectToAnserPage: SSE error: ", error);
-  };
-  sse.addEventListener('init', function (event) {
-    console.log("SseAutoRedirectToAnserPage: Init event received: " + event.data);
-  });
-  sse.onmessage = function(event) {
-    console.log("SseAutoRedirectToAnserPage: sse.onmessage Event received: " + event.data);
+  run() {
+    console.log("SseAutoRedirectToAnswerPage.run() is called. ")
 
-    try {
-      if(!isNaN(event.data)) {
+    try { this.sse.close(); } catch(e) { console.warn("SseAutoRedirectToAnswerPage.run():  ", e); }
+
+    this.sse = new EventSource("/sse/transitToAnswer");
+    this.sse.onopen = function() {
+      console.info("SseAutoRedirectToAnswerPage.run(): SSE connection opened. ");
+    };
+    this.sse.onerror = function (error) {
+      console.error("SseAutoRedirectToAnswerPage.run(): SSE error: ", error);
+    };
+    this.sse.addEventListener('init', function (event) {
+      console.log("SseAutoRedirectToAnswerPage.run(): Init event received: " + event.data);
+    });
+    this.sse.onmessage = function(event) {
+      console.log("SseAutoRedirectToAnswerPage.run(): sse.onmessage Event received: " + event.data);
+
+      try {
         const loc = "/playing/answer?room=" + event.data;
         console.log("Redirect to " + loc);
         window.location.href = loc;
-      }
 
-    } catch(e) {
-      console.error("Error by " + e);
-    }
-  };
+      } catch(e) {
+        console.error("Error by " + e);
+      }
+    };
+  }
+
+  close() {
+    console.log("SseAutoRedirectToAnswerPage.close() is called. ")
+    try { this.sse.close(); } catch { console.error("SseAutoRedirectToAnswerPage.close():  ", e); }
+  }
 }
